c7d31d2c6d266db20eb579e8fa386150b76f92fd
alvarez.peter.14 <alvarez.peter.14@0f0e2727-633e-0410-8115-1154c34994d1>
2007-11-08 13:33:35 +0000

    Removed file/folder

--- trunk/src/edu/gatech/cs6300/Constants.java
+++
@@ -1,7 +0,0 @@
-package edu.gatech.cs6300;
-
-public interface Constants {
-	  static final String USERNAME = "<your username>@thep.in";
-	  static final String PASSWORD = "<your password>";
-	  static final String GRADES_DB = "GradesDatabaseTeam<your team number>";
-}
